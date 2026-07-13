c2dca3bd64a51c3505d9f17aad8a4e9150983d47
Julian McNeill <julesdowork@gmail.com>
2026-07-13 18:52:41 -0400

    Mathf.Clamp() Ship Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float controlSpeed = 10f;
+    [SerializeField] private float xClampRange = 20f;
+    [SerializeField] private float yClampRange = 20f;
 
     private Vector2 movement;
 
@@ -15,9 +17,14 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
-        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
 
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0);
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0);
     }
 
     public void OnMove(InputValue value)
